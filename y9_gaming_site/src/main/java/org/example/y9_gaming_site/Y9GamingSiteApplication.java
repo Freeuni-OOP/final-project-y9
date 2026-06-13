cd47084c420b642fc0165da1755113412bdbd01d
keti gordeziani <kgord23@freeuni.edu.ge>
2026-06-13 17:40:21 +0400

    Initial project setup

---
+++ y9_gaming_site/src/main/java/org/example/y9_gaming_site/Y9GamingSiteApplication.java
@@ -0,0 +1,13 @@
+package org.example.y9_gaming_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Y9GamingSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Y9GamingSiteApplication.class, args);
+    }
+
+}
---
+++ y9_gaming_site/src/test/java/org/example/y9_gaming_site/Y9GamingSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.y9_gaming_site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Y9GamingSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
