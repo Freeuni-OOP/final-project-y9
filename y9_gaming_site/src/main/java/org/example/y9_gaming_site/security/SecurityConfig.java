e9795a1fe356d05069097656f04c9cfb001af698
marikhakhishvili <mkhak23@freeuni.edu.ge>
2026-07-07 06:35:29 +0400

    conflicti gavaswore

--- y9_gaming_site/src/main/java/org/example/y9_gaming_site/security/SecurityConfig.java
+++ y9_gaming_site/src/main/java/org/example/y9_gaming_site/security/SecurityConfig.java
@@ -37,6 +37,8 @@ public class SecurityConfig {
                         .requestMatchers("/streak/**").authenticated()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .requestMatchers("/home", "/stats/home").authenticated()
+                        .requestMatchers("/sudoku", "/sudoku.html").authenticated()
+                        .requestMatchers("/api/sudoku/**").authenticated()
                         .requestMatchers("/quizzes", "/quizzes/**", "/quizzes.html", "/addQuiz.html", "/api/quizzes/new").authenticated()
                         .requestMatchers("/api/quizzes/**").authenticated()
                         .requestMatchers("/profile", "/profile/**", "/profile.html").authenticated()
